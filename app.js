5b75bf9738b96895f0d8e8b2998c32b34bacaa46
Anurag Mishra <97835032+shaktimaan00@users.noreply.github.com>
2022-12-28 11:35:45 +0530

    ssssf

--- app.js
+++ app.js
@@ -17,13 +17,26 @@ experience.addEventListener("click", () => {
   ex_cols.classList.add("show")
 });
 
-console.log(ed_cols);
+// console.log(ed_cols);
 
+let pdot1 = document.querySelector(".pdot1");
+let pdot2 = document.querySelector(".pdot2");
+let box1 = document.querySelector(".box1");
+let box2 = document.querySelector(".box2");
 
+pdot1.addEventListener("click", () => {
+  pdot1.classList.add("pactive");
+  box1.style.display = "block";
+  pdot2.classList.remove("pactive");
+  box2.style.display = "none";
+})
 
-
-
-
+pdot2.addEventListener("click", () => {
+  pdot2.classList.add("pactive");
+  box2.style.display = "block";
+  pdot1.classList.remove("pactive");
+  box1.style.display = "none";
+});
 
 
 
